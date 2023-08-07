dc91a977f2b105fe0cc1432875fcd89843623714
faryd ignacio ortíz <faryd257@gmail.com>
2023-08-06 21:47:39 -0300

    m4u4

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', async (req, res, next) => {
+
+  console.log(req.body)// estoy capturando datos??
+
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var telefono = req.body.telefono;
+  var mensaje = req.body.mensaje;
+
+  var obj = {
+
+    to: 'faryd257@gmail.com',
+    subject: 'contacto desde la web',
+    html: nombre + " " + apellido + " se contacto a traves y quiere mas info a este correo:"
+      + email + ". <br> ademas, hizo el siguiente comentario:" + mensaje + ". <br> su telefono es" + telefono
+  } // cierra var obj
+
+  var transporter = nodemailer.createTransport({
+
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  }) // cierra transporter
+
+
+
+ var info = await transporter.sendMail(obj);
+
+  res.render('index', { message: 'mensaje enviado correctamente', });
+
+
+});// cierra peticion del post
+
+module.exports = router;
