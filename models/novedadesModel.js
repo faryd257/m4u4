006b603a9b7b148a793ac6264dd2540a99ab243b
faryd ignacio ortíz <faryd257@gmail.com>
2023-08-12 01:11:20 -0300

    m5u1

---
+++ models/novedadesModel.js
@@ -0,0 +1,19 @@
+const pool = require('./bd');
+
+/* sirve para listar las novedades*/
+
+
+
+async function getnovedades() {
+    var query = 'select * from novedades';
+    var rows = await pool.query(query);
+    return rows;
+}
+
+async function deleteNovedadesById(id) {
+    var query = 'delete from novedades where id = ?';
+    var rows = await pool.query(query, [id]);
+    return rows;
+}
+
+module.exports = { getnovedades, deleteNovedadesById }
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
-router.get('/', function (req, res, next) {
+/* GET NOVEDADES PAGINA*/
+router.get('/', async function (req, res, next) {
+
+  var novedades = await novedadesModel.getnovedades();
   res.render('admin/novedades', {
     layout: 'admin/layout',
-    persona: req.session.nombre
+    persona: req.session.nombre,
+    novedades
   });
 });
+
+//Para eliminar una novedad/
+
+router.get('/eliminar/:id', async (req, res, next) => {
+  const id = req.params.id; //2
+
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades')
+}); // cierra get de eliminar
 
 module.exports = router;
