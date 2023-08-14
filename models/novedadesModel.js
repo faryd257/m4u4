7fa9ce83a064eb0fc178a2456f8f80e9a864852f
faryd ignacio ortíz <faryd257@gmail.com>
2023-08-13 23:22:46 -0300

    modulo5unidad2

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -16,4 +16,36 @@ async function deleteNovedadesById(id) {
     return rows;
 }
 
-module.exports = { getnovedades, deleteNovedadesById }
+
+async function insertNovedad(obj) {
+    try {
+        var query = "insert into novedades set ?";
+        var rows = await pool.query(query, [obj])
+        return rows;
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    } // cierra catch
+} //cierra insert
+
+/* para modificar que me traiga una sola novedad por ir*/
+async function getNovedadById(id) {
+    var query = "select * from novedades where id=? ";
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+/*para modificar > que actualice esos campos por el id */
+
+async function modificarNovedadById(obj, id) {
+    try {
+        var query = "update novedades set ? where id=? ";
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+} // cierra modi >update
+
+module.exports = { getnovedades, deleteNovedadesById, insertNovedad, getNovedadById, modificarNovedadById }
